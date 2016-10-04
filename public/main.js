14a606bf6e871a6325e5a6701e2f2bae22ddd49b
Jack Mocherman <mochermanjack@gmail.com>
2016-10-04 15:28:46 -0500

    fully functioning tic tac toe built on front end

--- public/main.js
+++ public/main.js
@@ -10,11 +10,16 @@ socket.on('disconnect', () => {
 	console.log(`Socket disconnected`)
 })
 
+const table = document.querySelector('.board')
+const status = document.querySelector('.status')
+
 const boardState = [
 	['','',''],
 	['','',''],
 	['','','']
 ]
+
+let nextPlayer = 'X'
 
 const drawBoard = (boardState) => {
 	document.querySelector('.board').innerHTML = `
@@ -36,12 +41,52 @@ const drawBoard = (boardState) => {
 			</tr>
 		</table>
 	`
+	status.innerText = `${nextPlayer}'s Turn`
+}
+
+const winner = b => {
+  // Rows
+  if (b[0][0] && b[0][0] === b[0][1] && b[0][1] === b[0][2]) {
+    return b[0][0]
+  }
+
+  if (b[1][0] && b[1][0] === b[1][1] && b[1][1] === b[1][2]) {
+    return b[1][0]
+  }
+
+  if (b[2][0] && b[2][0] === b[2][1] && b[2][1] === b[2][2]) {
+    return b[2][0]
+  }
+
+  // Cols
+  if (b[0][0] && b[0][0] === b[1][0] && b[1][0] === b[2][0]) {
+    return b[0][0]
+  }
+
+  if (b[0][1] && b[0][1] === b[1][1] && b[1][1] === b[2][1]) {
+    return b[0][1]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][2] && b[1][2] === b[2][2]) {
+    return b[0][2]
+  }
+
+  // Diags
+  if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
+    return b[0][0]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
+    return b[0][2]
+  }
+
+  // Tie or In-Progress
+  else {
+    return null
+  }
 }
 
 drawBoard(boardState)
-let nextPlayer = 'X'
-
-const table = document.querySelector('.board')
 
 table.addEventListener('click', e => {
 	const col = e.target.cellIndex
@@ -51,8 +96,18 @@ table.addEventListener('click', e => {
 		return console.log('Cannot move there')
 	}
 
+	if (winner(boardState)) {
+		return console.log('Game is over!')
+	}
+
 	boardState[row][col] = nextPlayer
-	nextPlayer = nextPlayer === 'X' ? 'O' : 'X'
 	drawBoard(boardState)
 	console.log('You clicked on :', row, col)
+
+	if (winner(boardState)) {
+    return status.innerText = `${nextPlayer} WON!`
+  }
+
+	nextPlayer = nextPlayer === 'X' ? 'O' : 'X'
+	status.innerText = `${nextPlayer}'s Turn!`
 })
