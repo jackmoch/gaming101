0e4bc671feed98c5f936c18b4795fc8d69c7aebd
Jack Mocherman <mochermanjack@gmail.com>
2016-10-04 14:24:25 -0500

    serving / path with mongo connection to localhost 3000

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const { Server } = require('http')
+const mongoose = require('mongoose')
 const socketio = require('socket.io')
 
 const app = express()
@@ -9,5 +10,16 @@ const server = Server(app)
 const io = socketio(server)
 
 const PORT = process.env.PORT || 3000
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/ticktacktoe'
 
-server.listen(PORT, () => console.log(`Server listening on por ${PORT}`))
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+mongoose.connect(MONGODB_URL, () => {
+	server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+})
